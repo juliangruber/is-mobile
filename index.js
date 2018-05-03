1d46063a2f871440d35960bf99a343910c5d8919
Sergey Egorov <es@elcom.ru>
2018-05-03 14:55:19 +0300

    export as an isMobile obj
    to have an opportunity import module to TypeScript 
    as
    import {isMobile} from 'is-mobile';

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+'use strict'
+
 module.exports = isMobile
+module.exports.isMobile = isMobile
 
 var mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
 
