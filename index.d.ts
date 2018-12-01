fae8fcda34b6aa5c42b4c8e33aa86f4d3ade5a18
xobotyi <xog3@yandex.ru>
2018-12-01 17:57:15 +0300

    Actualized typings;

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,16 @@
-export declare function isMobile(ua?: string|object, opts?: object): boolean;
+interface Headers {
+    [id: string]: string | string[]
+}
+
+interface Request {
+    headers: Headers,
+
+    [id: string]: any,
+}
+
+interface InputOptions {
+    tablet?: boolean,
+    ua?: string | Request
+}
+
+export default function isMobile(opt?: InputOptions): boolean;
