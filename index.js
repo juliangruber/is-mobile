e8b6b384fdb53943032b256a35a54dd5d66053c2
xobotyi <xog3@yandex.ru>
2018-12-01 17:56:52 +0300

    Proper ESM support (default export); For node - better to check navigator in `global` object;

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-module.exports = isMobile
-module.exports.isMobile = isMobile
+module.exports = isMobile;
+module.exports.default = isMobile;
 
 var mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
 
@@ -10,7 +10,7 @@ var tabletRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|c
 function isMobile (opts) {
   if (!opts) opts = {}
   var ua = opts.ua
-  if (!ua && typeof navigator !== 'undefined') ua = navigator.userAgent
+  if (!ua && global.navigator) ua = global.navigator.userAgent
   if (ua && ua.headers && typeof ua.headers['user-agent'] === 'string') {
     ua = ua.headers['user-agent']
   }
