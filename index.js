ff8c0769fd6c402a44bb7cafb7d4d56b84734790
a.zinoviev <a.zinoviev@gaijin.ru>
2019-07-25 15:23:45 +0300

    default exports from ESM support;

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = isMobile
 module.exports.isMobile = isMobile
+module.exports.default = isMobile
 
 var mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
 
