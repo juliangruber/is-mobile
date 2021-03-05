152af915d5fa53a7a43f0dbaf6399e48d034d91b
Anton Golub <golub.anton@gmail.com>
2021-03-05 14:06:56 +0300

    feat: detect armv7l-based mobile devices (#30)
    * perf: cut tableRE, save some extra bytes
    
    * feat: detect armv7l-based mobile devices
    
    * fix(pkg): up deps, fix vulnerabilities
    
    * refactor: code improvements

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ module.exports = isMobile
 module.exports.isMobile = isMobile
 module.exports.default = isMobile
 
-var mobileRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
+const mobileRE = /(android|bb\d+|meego).+mobile|armv7l|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
 
-var tabletRE = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series[46]0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|android|ipad|playbook|silk/i
+const tabletRE = /android|ipad|playbook|silk/i
 
 function isMobile (opts) {
   if (!opts) opts = {}
-  var ua = opts.ua
+  let ua = opts.ua
   if (!ua && typeof navigator !== 'undefined') ua = navigator.userAgent
   if (ua && ua.headers && typeof ua.headers['user-agent'] === 'string') {
     ua = ua.headers['user-agent']
   }
   if (typeof ua !== 'string') return false
 
-  var result = opts.tablet ? tabletRE.test(ua) : mobileRE.test(ua)
+  let result = mobileRE.test(ua) || (!!opts.tablet && tabletRE.test(ua))
 
   if (
     !result &&
--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-var test = require('tape')
-var isMobile = require('./')
+const test = require('tape')
+const describe = require('tape-describe')
+const UserAgent = require('user-agents')
+const isMobile = require('./')
 
-var iphone =
+const iphone =
   'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3'
-var chrome =
+const chrome =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36'
-var ffos = 'Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0'
-var ipad =
+const ffos = 'Mozilla/5.0 (Mobile; rv:18.0) Gecko/18.0 Firefox/18.0'
+const ipad =
   'Mozilla/5.0 (iPad; CPU OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1'
-var ios13ipad =
+const ios13ipad =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
-var ios13ipadpro =
+const ios13ipadpro =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15'
 
 test('is mobile', function (t) {
@@ -44,4 +46,31 @@ test('is mobile', function (t) {
   t.ok(isMobile({ ua: ios13ipadpro, tablet: true, featureDetect: true }))
 
   t.end()
+})
+
+describe('ua-bruteforce', function () {
+  const limit = 300
+  const checks = {
+    mobile: { result: true },
+    tablet: { result: true, tablet: true },
+    desktop: { result: false }
+  }
+  const testCases = Object.entries(checks).reduce(
+    (cases, [deviceCategory, { result, tablet }]) => [
+      ...cases,
+      ...new Array(limit).fill().map(() => ({
+        ua: new UserAgent({ deviceCategory }).toString(),
+        result,
+        tablet
+      }))
+    ],
+    []
+  )
+
+  testCases.forEach(({ ua, result, tablet }) => {
+    test(ua, t => {
+      t.equal(isMobile({ ua, tablet }), result)
+      t.end()
+    })
+  })
 })
